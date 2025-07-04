854e4fef90e8875719685f56b36d18d51eed49a7
ag-444 <aditya444garg@gmail.com>
2025-07-04 20:22:09 +0530

    T41.16 feat: Update Navbar component with button links for Sign In and Sign Up; adjust navbar height

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,12 @@ import { NAVBAR_HEIGHT } from '@/lib/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 const Navbar = () => {
   return (
     <div
-        className='fixed top-0 left-0 w-full  z-50 shadow-xl'
+        className="fixed top-0 left-0 w-full z-50 shadow-md"
         style={{ height: `${NAVBAR_HEIGHT}px` }}
     >
         <div className='flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white' >
@@ -31,9 +32,28 @@ const Navbar = () => {
                             </div>
                         </div>
                 </Link>
-                <p className='text-primary-200 hidden md:block '>
+                
+            </div>
+            <p className='text-primary-200 hidden md:block '>
                 Discover your next home with ease
-                </p>
+            </p>
+            <div className='flex items-center gap-5'>
+                <Link href="/signin">
+                    <Button
+                        variant='outline'
+                        className='text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-lg '
+                    >
+                        Sign In
+                    </Button>
+                </Link>
+                <Link href="/signup">
+                    <Button
+                        variant='outline'
+                        className='text-white bg-secondary-600 hover:bg-white  hover:text-primary-700 rounded-lg '
+                    >
+                        Sign Up
+                    </Button>
+                </Link>
             </div>
         </div>
     </div>
